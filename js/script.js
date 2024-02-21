7c7627ebc98e4e1bb93420c37825077006927233
post-coder <gabrielspanu.dev@gmail.com>
2024-02-21 09:45:08 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/*
+
+*/
+
+
+const {createApp} = Vue;
+
+createApp({
+    data() {
+        return {
+
+            tasks: [
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                }
+            ]
+
+        }
+    },
+
+    methods: {
+        
+    },
+}).mount("#app");
