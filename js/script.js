bee4521b3e69233fc1a61eb5eb1ae5374bade64f
post-coder <gabrielspanu.dev@gmail.com>
2024-02-21 10:19:35 +0100

    refactor: ottimizzazione aggiunta task

--- js/script.js
+++ js/script.js
@@ -24,7 +24,10 @@ createApp({
                 }
             ],
 
-            newTaskText: '',
+            newTask: {
+                done: false,
+                text: ''
+            },
 
         }
     },
@@ -42,26 +45,30 @@ createApp({
 
         addTask() {
             // contenuto del campo di testo
-            // console.log(this.newTaskText)
+            // console.log(this.newTask.text)
 
-            // creo un nuovo oggetto task
-            const newTask = {
-                done: false
+            // se provassi a fare il push direttamente del newTask
+            // vedrei in pagina che alla modifica del campo di input
+            // si modificherebbe magicamente anche il testo del task appena inserito in lista
+            // this.tasks.push(this.newTask)
+
+            // per evitare ciò, usiamo lo spread operator
+            // questo "rimuove le parentesi" che circondano un oggetto (o array)
+            // e inserisce le proprietà dentro un nuovo oggetto, in questo modo
+            // interrompo il legame
+
+            // controllo che non stia aggiungendo un task vuoto
+            // il metodo .trim() di una stringa elimina da quella stringa gli spazi vuoti
+            // in questo modo se ho inserito solo degli spazi vuoti, verranno eliminati prima di fare il controllo
+            if(this.newTask.text.trim() != "") {
+
+                // aggiungo il task all'array
+                this.tasks.push({...this.newTask})
+                console.log(this.newTask)
+    
+                // resetto il testo del nuovo task
+                this.newTask.text = ""
             }
-
-            // inserisco il testo all'interno della sua proprietà "text"
-            newTask.text = this.newTaskText
-
-
-            // lo inserisco nell'array
-            this.tasks.push(newTask)
-
-            console.log(this.tasks)
-
-            // resetto il contenuto del campo di input
-            this.newTaskText = ""
-
         },
-
     },
 }).mount("#app");
